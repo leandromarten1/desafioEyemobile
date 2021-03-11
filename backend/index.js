e799ee83fe43e9ad59b50bb9caf0df7d3e932a1f
leandromarten1 <leandromarten1@gmail.com>
2021-03-11 09:56:35 -0300

    sequelize

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const path = require('path')
 const app = express();
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.get('/', (_req, res) => res.send('Hello World'));
 
